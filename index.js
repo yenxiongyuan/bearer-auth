3e50dc03a8fbc51e82df46f1d14d4599b807ee4d
Yen Xiong Yuan <yenyuan17@gmail.com>
2023-03-08 02:40:42 +0000

    worked on lab 7

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 'use strict';
 
 // Start up DB Server
-const { db } = require('./src/auth/models/index.js');
+const { db } = require('./src/auth/models/index.js'); 
 db.sync()
   .then(() => {
-
     // Start the web server
     require('./src/server.js').start(process.env.PORT);
   });
-
